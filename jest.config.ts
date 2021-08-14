98308aae9876c5aecf8828ce2f6b5069dbd0b719
kujikeo <guilhermelima_1996@hotmail.com>
2021-08-14 17:10:53 -0300

    chore: add jest to lint staged

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ export default {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  testEnvironment: 'jest-environment-node',
   coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
