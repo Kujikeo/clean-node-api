c1e61296a49ade2fbb0cbfe77d57ba7d43c32c56
kujikeo <guilhermelima_1996@hotmail.com>
2021-08-15 13:43:34 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
+import {HttpResponse, HttpResquest} from '../protocols/http'
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpResquest): HttpResponse {
     if(!httpRequest.body.name){
       return{
         statusCode: 400, 
@@ -12,6 +13,5 @@ export class SignUpController {
         body: new Error('Miussing param: email')
       }
     }
-
   }
 }
